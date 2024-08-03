f691c2b2d432132cddb950215b988b91ed2201f7
Sabesh-Adithya <sabeshadi555@gmail.com>
2024-08-03 19:06:57 +0530

    new file final

--- first.js
+++ first.js
@@ -1,7 +1,12 @@
 const bar =document.getElementById('bar');
 const close =document.getElementById('close');
 const nav =document.getElementById('navbar');
-if(bar){
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active1');
+    })
+}
+if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active1');
     })
